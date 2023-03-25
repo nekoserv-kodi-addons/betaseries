2a5c6a8a18db7723d2d69ba884ac534558ce913e
nekoserv-kodi-addons <nekoserv-kodi-addons@domain.tld>
2023-03-25 10:17:53 +0100

    Scripts : overwrite previous releases, misc

--- scripts/release-repo.sh
+++ scripts/release-repo.sh
@@ -8,18 +8,47 @@ user="nekoserv-kodi-addons"
 repo="betaseries"
 create_release_url="https://api.github.com/repos/$user/$repo/releases"
 
+
 ## remove previous tag
-echo " -> Deleting previous tag"
+echo -n " -> Deleting previous tag : "
 curl \
   -s \
+  --fail \
   -X DELETE \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $token" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/$user/$repo/git/refs/tags/$tag_name"
+ "https://api.github.com/repos/$user/$repo/git/refs/tags/$tag_name"
+if [ $? -eq 0 ]; then echo "OK"; else echo "failed"; fi
+
+
+echo -n " -> Finding previous release : "
+release_id=$(curl \
+  -so- \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $token" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/$user/$repo/releases/tags/$tag_name" | awk '/"id":/ {print; exit;}' | sed -E 's/^[^0-9]*([0-9]+).*/\1/')
+
+## remove previous release
+if [ -z "$release_id" ]; then
+	echo "KO (no previous release)"
+else
+	echo "OK"
+	echo -n " -> Deleting release $release_id : "
+	curl \
+	  -s \
+	  -X DELETE \
+	  -H "Accept: application/vnd.github+json" \
+	  -H "Authorization: Bearer $token" \
+	  -H "X-GitHub-Api-Version: 2022-11-28" \
+	  https://api.github.com/repos/$user/$repo/releases/$release_id
+	echo "OK"
+fi
+
 
 ## create release and get id
-echo -n "\n -> Creating new release\n"
+echo -n " -> Creating new release : "
 release_id=$(curl \
   -so- \
   -X POST \
@@ -28,14 +57,17 @@ release_id=$(curl \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -d '{"tag_name":"'$tag_name'","name":"repository '$version'"}' \
   "$create_release_url" | awk '/"id":/ {print; exit;}' | sed -E 's/^[^0-9]*([0-9]+).*/\1/')
-echo -n "\n -> release_id is: $release_id\n"
+echo "$release_id"
+
 
 ## create archive
 archive_name="$tag_name-${version#?}.zip"
 cd repository/; zip -qr ../$archive_name repository.betaseries
 cd - > /dev/null
 
+
 ## add archive asset
+echo -n " -> asset_id #1 is : "
 add_asset_url="https://uploads.github.com/repos/$user/$repo/releases/$release_id/assets?name=$archive_name"
 asset_id=$(curl \
   -so- \
@@ -45,9 +77,11 @@ asset_id=$(curl \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   --data-binary "@$archive_name" \
   "$add_asset_url" | awk '/"id":/ {print; exit;}' | sed -E 's/^[^0-9]*([0-9]+).*/\1/')
-echo "\n -> asset_id #1 is: $asset_id"
+echo "$asset_id"
+
 
 ## add icon asset
+echo -n " -> asset_id #2 is : "
 asset_name="resources/icon.png"
 add_asset_url="https://uploads.github.com/repos/$user/$repo/releases/$release_id/assets?name=${asset_name##*/}"
 asset_id=$(curl \
@@ -58,4 +92,4 @@ asset_id=$(curl \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   --data-binary "@$asset_name" \
   "$add_asset_url" | awk '/"id":/ {print; exit;}' | sed -E 's/^[^0-9]*([0-9]+).*/\1/')
-echo "\n -> asset_id #2 is: $asset_id"
+echo "$asset_id"
--- scripts/release.sh
+++ scripts/release.sh
@@ -9,17 +9,45 @@ repo="betaseries"
 create_release_url="https://api.github.com/repos/$user/$repo/releases"
 
 ## remove previous tag
-echo " -> Deleting previous tag"
+echo -n " -> Deleting previous tag : "
 curl \
   -s \
+  --fail \
   -X DELETE \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $token" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/$user/$repo/git/refs/tags/$tag_name"
+if [ $? -eq 0 ]; then echo "OK"; else echo "failed"; fi
+
+
+echo -n " -> Finding previous release : "
+release_id=$(curl \
+  -so- \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $token" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/$user/$repo/releases/tags/$tag_name" | awk '/"id":/ {print; exit;}' | sed -E 's/^[^0-9]*([0-9]+).*/\1/')
+
+## remove previous release
+if [ -z "$release_id" ]; then
+	echo "KO (no previous release)"
+else
+	echo "OK"
+	echo -n " -> Deleting release $release_id : "
+	curl \
+	  -s \
+	  -X DELETE \
+	  -H "Accept: application/vnd.github+json" \
+	  -H "Authorization: Bearer $token" \
+	  -H "X-GitHub-Api-Version: 2022-11-28" \
+	  https://api.github.com/repos/$user/$repo/releases/$release_id
+	echo "OK"
+fi
+
 
 ## create release and get id
-echo -n "\n -> Creating new release\n"
+echo -n " -> Creating new release : "
 release_id=$(curl \
   -so- \
   -X POST \
@@ -28,7 +56,8 @@ release_id=$(curl \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -d '{"tag_name":"'$tag_name'","name":"betaseries '$version'"}' \
   "$create_release_url" | awk '/"id":/ {print; exit;}' | sed -E 's/^[^0-9]*([0-9]+).*/\1/')
-echo -n "\n -> release_id is: $release_id\n"
+echo "$release_id"
+
 
 ## create archive
 archive_name="$tag_name-${version#?}.zip"
@@ -36,7 +65,9 @@ cd ../; mv "$repo" "$tag_name";
 zip -qr "$archive_name" "$tag_name" -x "*/.git*" "*/repository/*" "*/scripts/*"
 mv "$tag_name" "$repo"
 
+
 ## add archive asset
+echo -n " -> asset_id #1 is : "
 add_asset_url="https://uploads.github.com/repos/$user/$repo/releases/$release_id/assets?name=$archive_name"
 asset_id=$(curl \
   -so- \
@@ -46,9 +77,11 @@ asset_id=$(curl \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   --data-binary "@$archive_name" \
   "$add_asset_url" | awk '/"id":/ {print; exit;}' | sed -E 's/^[^0-9]*([0-9]+).*/\1/')
-echo "\n -> asset_id #1 is: $asset_id"
+echo "$asset_id"
+
 
 ## add icon asset
+echo -n " -> asset_id #2 is : "
 asset_name="resources/icon.png"
 add_asset_url="https://uploads.github.com/repos/$user/$repo/releases/$release_id/assets?name=${asset_name##*/}"
 asset_id=$(curl \
@@ -59,4 +92,4 @@ asset_id=$(curl \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   --data-binary "@$asset_name" \
   "$add_asset_url" | awk '/"id":/ {print; exit;}' | sed -E 's/^[^0-9]*([0-9]+).*/\1/')
-echo "\n -> asset_id #2 is: $asset_id"
+echo "$asset_id"
