d0ac117966457648ef39bad56c4e754ea47d9267
nekoserv-kodi-addons <nekoserv-kodi-addons@domain.tld>
2023-03-26 20:25:08 +0200

    testing

--- betaseries/tests/subtitles/test_setting_service.py
+++ betaseries/tests/subtitles/test_setting_service.py
@@ -1,25 +1,22 @@
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
+from betaseries.settings.setting_enum import SettingEnum
 from betaseries.settings.setting_service import SettingService
-
-
-def mock_addon_method(item):
-    return item
 
 
 class TestSettingService(TestCase):
 
-    @patch('betaseries.settings.setting_service.Addon.getAddonInfo', MagicMock(return_value=mock_addon_method))
-    def test_get(self, mock_addon):
-        # mock_addon.getAddonInfo.return_value = mock_addon_method
-        # mock_addon.getLocalizedString.return_value = mock_addon_method('locale')
-        # mock_addon.getSetting.return_value = mock_addon_method
+    def mock_addon_method(self, item):
+        return item
 
-        param_list = [
-            'profile', 'id', 'name', 'locale', 'icon', 'notify', 'dirsync', 'uifirst', 'ccfirst', 'betaseries_apikey',
-            'tmdb_apikey'
-        ]
-        for item in param_list:
+    @patch('betaseries.settings.setting_service.Addon.getAddonInfo', new=mock_addon_method)
+    @patch('betaseries.settings.setting_service.Addon.getSetting', new=mock_addon_method)
+    @patch('betaseries.settings.setting_service.Addon.getLocalizedString', 'lang')
+    @patch('betaseries.settings.setting_service.Addon.getSetting', new=mock_addon_method)
+    # @patch('betaseries.settings.setting_service.translatePath', MagicMock(return_value='profile'))
+    def test_get(self):
+
+        for item in SettingEnum:
             with self.subTest(item):
-                self.assertEqual(item, SettingService.get(item))
+                self.assertEqual(item.name.lower(), SettingService.get(item))
